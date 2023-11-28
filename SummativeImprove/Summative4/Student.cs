9d0afafcee31e7a099c07fcf1d9094d21741cd88
Rechlally <Sam@eastwind.karoo.co.uk>
2023-11-28 11:33:32 +0000

    Improved class

--- SummativeImprove/Summative4/Student.cs
+++ SummativeImprove/Summative4/Student.cs
@@ -1,3 +1,54 @@
-﻿namespace Summative4;
+﻿using System.Text.RegularExpressions;
 
-public record Student();
+namespace Summative4;
+
+public class Student
+{
+    //student info
+    public int id { get; }
+    public string lName { get; }
+    public string fName { get; }
+    //student performance
+    private int[] challenges;
+    public int Challenges { get; }
+    public int project { get; }
+    public int exam { get; }
+    
+    public Student(string line)
+    {
+        string data = line;
+        Regex regex = new Regex(@"Student:\[ID:(\d+),lastName:(\w+),FirstName:(\w+)\]Marks:\[Challenges:\[([0-9]+)\],Exam:(\d+),Capstone:(\d+)\]");
+        Match match = regex.Match(data);
+        if(match.Success)
+        {
+            id = int.Parse(match.Groups[1].Value);
+            lName = match.Groups[2].Value;
+            fName = match.Groups[3].Value;
+            challenges = match.Groups[4].Value.Split(',').Select(int.Parse).ToArray();
+            exam = int.Parse(match.Groups[5].Value);
+            project = int.Parse(match.Groups[6].Value);
+            Challenges = getChallengeScore(challenges);
+        }
+        
+    }
+
+    private int getChallengeScore(int[] scores)
+    {
+        int[] firstChal = new int[5];
+        int[] lastChal = new int[4];
+        for (int index = 0; index < 5; index++)
+        {
+            firstChal[index] = scores[index];
+        }
+
+        for (int index = 5; index < 9; index++)
+        {
+            lastChal[index - 5] = scores[index];
+        }
+        Array.Sort(firstChal);
+        Array.Sort(lastChal);
+        Array.Reverse(firstChal);
+        Array.Reverse(lastChal);
+        return firstChal[0] + firstChal[1] + firstChal[2] + firstChal[3] + lastChal[0] + lastChal[1] + lastChal[2];
+    }
+}
